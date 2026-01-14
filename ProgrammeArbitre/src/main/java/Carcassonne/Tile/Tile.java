3e7a0e352542e69cce3272b4a59462fb742715c5
Charonov Andrei <andrei.charonov.etu@a13p13.fil.univ-lille.fr>
2026-01-14 10:22:14 +0100

    feat: Tile rotation

--- ProgrammeArbitre/src/main/java/Carcassonne/Tile/Tile.java
+++ ProgrammeArbitre/src/main/java/Carcassonne/Tile/Tile.java
@@ -8,23 +8,32 @@ public class Tile {
     private Edge bottomEdge;
     private Edge leftEdge;
 
+    private Direction direction;
+    // Direction where the top Edge of the tile is.
+    // By default, it's on top.
+
     public Tile(Edge topEdge, Edge rightEdge, Edge bottomEdge, Edge leftEdge) {
         this.topEdge = topEdge;
         this.rightEdge = rightEdge;
         this.bottomEdge = bottomEdge;
         this.leftEdge = leftEdge;
+        this.direction = Direction.TOP;
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
+
+    public void changeDirection(Direction direction){
+        this.direction = direction;
     }
 
     public Edge getEdge(Direction direction) {
-        switch (direction) {
-            case Direction.TOP:
-                return topEdge;
-            case Direction.RIGHT:
-                return rightEdge;
-            case Direction.BOTTOM:
-                return bottomEdge;
-            default:
-                return leftEdge;
-        }
+        return switch (direction) {
+            case Direction.TOP -> topEdge;
+            case Direction.RIGHT -> rightEdge;
+            case Direction.BOTTOM -> bottomEdge;
+            default -> leftEdge;
+        };
     }
 }
