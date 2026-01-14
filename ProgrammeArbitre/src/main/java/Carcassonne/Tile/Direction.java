52ae32a7b4277c8f7df470a06f852477fd30ff95
Charonov Andrei <andrei.charonov.etu@a13p13.fil.univ-lille.fr>
2026-01-14 10:27:01 +0100

    feat: OppositeDirection in enum direction

--- ProgrammeArbitre/src/main/java/Carcassonne/Tile/Direction.java
+++ ProgrammeArbitre/src/main/java/Carcassonne/Tile/Direction.java
@@ -2,4 +2,13 @@ package Carcassonne.Tile;
 
 public enum Direction {
     TOP, RIGHT, BOTTOM, LEFT;
+
+    public Direction oppositeDirection(Direction direction){
+        return switch (direction) {
+            case Direction.TOP -> BOTTOM;
+            case Direction.RIGHT -> LEFT;
+            case Direction.BOTTOM -> TOP;
+            default -> RIGHT;
+        };
+    }
 }
