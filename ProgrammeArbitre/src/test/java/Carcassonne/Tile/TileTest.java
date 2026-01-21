5155cc445222c6f3ef2765512b8ec1b553668890
Andreï Charonov <andrei.charonov..etu@univ-lille.fr>
2026-01-21 09:52:39 +0100

    test: Connect two tiles without roads

--- ProgrammeArbitre/src/test/java/Carcassonne/Tile/TileTest.java
+++ ProgrammeArbitre/src/test/java/Carcassonne/Tile/TileTest.java
@@ -1,11 +1,23 @@
 package Carcassonne.Tile;
 
-import org.junit.jupiter.api.Test;
+import Carcassonne.Edge.*;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class TileTest {
     @Test
-    public void testTile(){
-        assertTrue(true);
+    public void testConnectTwoCompatibleTilesNoRoads(){
+        Tile tile1 = new Tile(new EdgeNoRoad(Zone.CITY), new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.FIELD));
+        Tile tile2 = new Tile(new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.CITY), new EdgeNoRoad(Zone.FIELD));
+
+        assertTrue(tile1.canConnectTo(tile2, Direction.TOP));
+    }
+
+    @Test
+    public void testConnectTwoIncompatibleTilesNoRoads(){
+        Tile tile1 = new Tile(new EdgeNoRoad(Zone.CITY), new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.FIELD));
+        Tile tile2 = new Tile(new EdgeNoRoad(Zone.CITY), new EdgeNoRoad(Zone.CITY), new EdgeNoRoad(Zone.FIELD), new EdgeNoRoad(Zone.CITY));
+
+        assertFalse(tile1.canConnectTo(tile2, Direction.TOP));
     }
 }
