0f38b41eea1dbb9c5d2ef9eea2830bd5437d7602
Andreï Charonov <andrei.charonov.etu@univ-lille.fr>
2026-03-11 09:48:51 +0100

    refactor: initialize game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,16 +1,25 @@
 import l3s6.projet.star.game.board.Board;
+import org.json.simple.parser.ParseException;
 import player.Player;
 import tile.Deck;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Game {
-    private final ArrayList<Player> players;
+    private final ArrayList<Player> players = new ArrayList<>();
     private Player playingPlayer;
     private Deck deck;
     private Board board;
 
-    public Game() {
-        this.players = new ArrayList<>();
+    public Game(String path) throws IOException, ParseException {
+        initializePlayers();
+        this.board = new Board();
+        this.deck = new Deck(path);
+        this.playingPlayer = players.get(0);
+    }
+
+    private void initializePlayers(){
+        //ToDo: Attendre la connection de tout les joueurs et les mettre dans players
     }
 }
